b3475a2772955ce103a0210512dc54bdb9860b14
VIJAYA GOPINADH REDDY VELAGALA <36162772+gopinadh5g7@users.noreply.github.com>
2020-02-20 11:51:59 +0530

    Add files via upload

---
+++ precache-manifest.975fb40315e32d7574d8e42b4ef4e311.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a494c529b013656e092441190b9da0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecc8bb3ae62aeb700127",
+    "url": "/static/css/main.16843f20.chunk.css"
+  },
+  {
+    "revision": "81941b7966253de61c61",
+    "url": "/static/js/2.385707dc.chunk.js"
+  },
+  {
+    "revision": "ecc8bb3ae62aeb700127",
+    "url": "/static/js/main.d75f52fd.chunk.js"
+  },
+  {
+    "revision": "f3dea6883819208352c1",
+    "url": "/static/js/runtime-main.fb6d99bb.js"
+  },
+  {
+    "revision": "94bed9ff0e5ab422b562c83b3fab5424",
+    "url": "/static/media/brandLogo.94bed9ff.png"
+  },
+  {
+    "revision": "e66a2d966a014e0b345882f792ef37bb",
+    "url": "/static/media/logo.e66a2d96.png"
+  },
+  {
+    "revision": "a7ccfe55b6948632c771a51f781f8f5e",
+    "url": "/static/media/profile.a7ccfe55.png"
+  }
+]);
